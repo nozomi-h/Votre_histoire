ae8d6883de69f393c86b724e54eb2f262d91e1e5
nozomi_h <hakamata.m14@gmail.com>
2019-11-07 02:28:24 +0900

    add jpostal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,25 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).on('turbolinks:load', function() {
+
+  $("#user_postal_code").jpostal({
+      // # 郵便番号の入力欄が１つの場合
+      // # 111-1111と1111111のどちらの入力形式でも住所を自動入力してくれる
+      postcode: [ "#user_postal_code" ],
+
+      // # 郵便番号の入力欄が3桁-4桁で分かれている場合
+      // # postcode : [ '#zipcode1', '#zipcode2' ]
+      // # 入力項目フォーマット
+      // #   %3  都道府県
+      // #   %4  市区町村
+      // #   %5  町域
+      // #   %6  大口事業所の番地
+      // #   %7  大口事業所の名称
+      address: {
+        "#user_prefecture": "%3", // # 都道府県が入力される
+        "#user_city_address": "%4%5", // # 市区町村と町域が入力される
+        "#user_building": "%6%7" // # 大口事務所の番地と名称が入力される
+      }
+  })
