3983fcce8d3fe9facc9a7de5c5046d9592d13220
nozomi_h <hakamata.m14@gmail.com>
2019-11-14 14:32:05 +0900

    gretel

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,6 +1,6 @@
 ## 情報入力
 crumb :root do
-  link "情報入力", new_cart_path
+  link "情報入力", new_user_cart_path
 end
 
 ## ご注文内容の確認
@@ -8,16 +8,16 @@ crumb :carts_index do
   link "ご注文内容の確認", carts_path
 end
 
-# ご注文完了
-# crumb :issue do |issue|
-#   link "ご注文完了", complete_order_path
-# end
+ご注文完了
+crumb :issue do |issue|
+  link "ご注文完了", complete_order_path
+end
 
 ## ご注文完了
-crumb :carts do
-  link "ご注文完了", new_cart_path
-  parent :carts_index
-end
+# crumb :carts do
+#   link "ご注文完了", new_user_cart_path
+#   parent :carts_index
+# end
 
 
 # crumb :projects do
